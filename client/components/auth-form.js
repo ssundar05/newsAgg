9302574d104b9af3219d8adb79871e9d95250f6e
Sunil Sundar <sunilsundar2@gmail.com>
2018-01-03 22:25:56 -0600

    styles updated

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,19 +1,67 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import { auth } from '../store'
 
 /**
  * COMPONENT
  */
 
- 
+
 const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+  const { name, displayName, handleSubmit, error } = props
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
+
+      <div id="fullDiv">
+        <section className="login-form-container">
+          <div className="card-container cCont">
+            <div className="card cCard">
+              <div className="card-header cHead">
+                <p>{displayName} Here</p>
+                {/* <a className="btn-floating btn-large waves-effect waves-light tooltipped" data-position="right" data-delay="50" data-tooltip="Register"><i className="material-icons">person_add</i></a> */}
+              </div>
+              <div className="card-content ccCont">
+                <form  onSubmit={handleSubmit} name={name}>
+                  <div className="row">
+                    <div className="input-field col l12 m12 s12">
+                      <i className="material-icons prefix">account_circle</i>
+                      <input placeholder="" id="email" type="text" className="validate" required="required" />
+                        <label htmlFor="email">Enter Username</label>
+                       </div>
+                    </div>
+                    <div className="row">
+                      <div className="input-field col l12 m12 s12">
+                        <i className="material-icons prefix">vpn_key</i>
+                        <input placeholder="" id="password" type="password" className="validate" required="required" />
+                          <label htmlFor="password">Password</label>
+                       </div>
+                      </div>
+                      <div className="row">
+                      <div className="input-field col l12 m12 s12 center-align">
+                        <button type = "submit" className="waves-effect waves-light btn blue-grey darken-1 z-depth-5">{displayName}</button>
+                      </div>
+                    </div>
+               </form>
+                    <div className="row">
+                      <div className="col l12 m12 s12">
+                        <div className="card-footerl">
+                          <div className="col l6 m6 s12 no-padding">
+                            <input type="checkbox" className="filled-in" id="remember-me" />
+                            <label htmlFor="remember-me">Remember Me</label>
+                          </div>
+                          <div className="col l6 m6 s12">
+                           
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+              </div>
+        </section>
+            {/* <form onSubmit={handleSubmit} name={name}>
         <div>
           <label htmlFor="email"><small>Email</small></label>
           <input name="email" type="text" />
@@ -27,9 +75,17 @@ const AuthForm = (props) => {
         </div>
         {error && error.response && <div> {error.response.data} </div>}
       </form>
-      <a href="/auth/google">{displayName} with Google</a>
+      <a href="/auth/google">{displayName} with Google</a> */}
+
+
+
+
+
+
+          </div>
+
     </div>
-  )
+        )
 }
 
 /**
@@ -41,7 +97,7 @@ const AuthForm = (props) => {
  */
 const mapLogin = (state) => {
   return {
-    name: 'login',
+          name: 'login',
     displayName: 'Login',
     error: state.user.error
   }
@@ -49,7 +105,7 @@ const mapLogin = (state) => {
 
 const mapSignup = (state) => {
   return {
-    name: 'signup',
+          name: 'signup',
     displayName: 'Sign Up',
     error: state.user.error
   }
@@ -57,8 +113,8 @@ const mapSignup = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleSubmit (evt) {
-      evt.preventDefault()
+          handleSubmit(evt) {
+        evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
@@ -74,7 +130,7 @@ export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
  * PROP TYPES
  */
 AuthForm.propTypes = {
-  name: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
   handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.object
