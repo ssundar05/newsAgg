c619fc66e41f2afa7ba0e54978163f69772e4100
Sunil Sundar <sunilsundar2@gmail.com>
2018-06-20 12:25:37 -0500

    yarn

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/newAgg', {
+  'postgres://yuufrweqfsktnj:c527d166149b11953dddc7a2bb2bdf877b92fe04c95d55a5a8b5a438e0ed042a@ec2-50-19-86-139.compute-1.amazonaws.com:5432/dfs9sfh1n96pi6' || 'postgres://localhost:5432/newAgg', {
     logging: false
   }
 )
